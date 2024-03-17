86d3409cab8396d94875282697b2eec547d64f3f
boggula-ramireddy <128015669+boggula-ramireddy@users.noreply.github.com>
2024-03-17 22:50:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,2 +1,21 @@
-# testWebsite2
-test
+import {Component} from 'react'
+import {Switch, Route, BrowserRouter} from 'react-router-dom'
+import LogIn from './component/Login'
+import Home from './component/Home'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
